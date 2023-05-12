5d25a493eebd6faf591545bca9e028aed6bc2fe2
jons111 <xlutfillayev24@gmail.com>
2023-05-12 17:10:45 +0500

    changed

--- data/config.py
+++ data/config.py
@@ -1,12 +1,17 @@
-from environs import Env
+# from environs import Env
+#
+# # environs kutubxonasidan foydalanish
+# env = Env()
+# env.read_env()
+#
+# # .env fayl ichidan quyidagilarni o'qiymiz
+# BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+# ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+# IP = env.str("ip")  # Xosting ip manzili
 
-# environs kutubxonasidan foydalanish
-env = Env()
-env.read_env()
-
-# .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-IP = env.str("ip")  # Xosting ip manzili
+import os
+BOT_TOKEN = str(os.path.join("BOT_TOKEN"))
+ADMINS = list(os.path.join("ADMINS"))
+IP = str(os.path.join("ip"))
 
 kanallar = ['@wwwww211','@kanal11232']
